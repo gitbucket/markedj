31ee911dc3ea6b934777f1c3c8ee01891e863dd4
Naoki Takezoe <takezoe@gmail.com>
2017-05-10 19:29:23 +0900

    (refs #1)Render HTML tags through whitelist

--- src/main/java/io/github/gitbucket/markedj/Marked.java
+++ src/main/java/io/github/gitbucket/markedj/Marked.java
@@ -1,5 +1,8 @@
 package io.github.gitbucket.markedj;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
 public class Marked {
 
     public static String marked(String src){
@@ -15,7 +18,20 @@ public class Marked {
         Lexer lexer = new Lexer(options);
         Lexer.LexerResult result = lexer.lex(src);
         Parser parser = new Parser(options, renderer);
-        return parser.parse(result.getTokens(), result.getLinks());
+        String html = parser.parse(result.getTokens(), result.getLinks());
+
+        Whitelist whitelist = Whitelist.relaxed();
+        whitelist.addAttributes("code", "class");
+        whitelist.addAttributes("th", "style");
+        whitelist.addAttributes("td", "style");
+        whitelist.addAttributes("h1", "id");
+        whitelist.addAttributes("h2", "id");
+        whitelist.addAttributes("h3", "id");
+        whitelist.addAttributes("h4", "id");
+        whitelist.addAttributes("h5", "id");
+        whitelist.addAttributes("h6", "id");
+
+        return Jsoup.clean(html, whitelist);
     }
 
 }
