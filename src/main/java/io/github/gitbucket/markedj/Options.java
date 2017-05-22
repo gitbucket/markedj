446df963efc9d8e4d2d67b4dd393a14f1c39766a
Naoki Takezoe <takezoe@gmail.com>
2017-05-22 11:14:51 +0900

    Update whitelist to keep links

--- src/main/java/io/github/gitbucket/markedj/Options.java
+++ src/main/java/io/github/gitbucket/markedj/Options.java
@@ -10,13 +10,26 @@ public class Options {
     private boolean sanitize = false;
     private String langPrefix = "lang-";
     private String headerPrefix = "";
-    private Whitelist whitelist = Whitelist.relaxed();
-    {
-        whitelist.addAttributes(":all", "id", "class", "style");
-        whitelist.addTags("input");
-        whitelist.addAttributes("input", "type", "checked", "name", "value", "disabled");
-        whitelist.addProtocols("a", "href", "#");
-    }
+    private Whitelist whitelist = new Whitelist()
+                .addTags(
+                        "a", "b", "blockquote", "br", "caption", "cite", "code", "col",
+                                "colgroup", "dd", "div", "dl", "dt", "em", "h1", "h2", "h3", "h4", "h5", "h6",
+                                "i", "img", "li", "ol", "p", "pre", "q", "small", "span", "strike", "strong",
+                                "sub", "sup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "u",
+                                "ul", "input")
+                .addAttributes("a", "href", "title")
+                .addAttributes("blockquote", "cite")
+                .addAttributes("col", "span", "width")
+                .addAttributes("colgroup", "span", "width")
+                .addAttributes("img", "align", "alt", "height", "src", "title", "width")
+                .addAttributes("ol", "start", "type")
+                .addAttributes("q", "cite")
+                .addAttributes("table", "summary", "width")
+                .addAttributes("td", "abbr", "axis", "colspan", "rowspan", "width")
+                .addAttributes("th", "abbr", "axis", "colspan", "rowspan", "scope", "width")
+                .addAttributes("ul", "type")
+                .addAttributes("input", "type", "checked", "name", "value", "disabled")
+                .addAttributes(":all", "id", "class", "style");
 
     public void setGfm(boolean gfm) {
         this.gfm = gfm;
